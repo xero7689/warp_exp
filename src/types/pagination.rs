e52e59b0a5a9e5dbc2930ffc9eb10d9790d396c1
xero7689 <volleyp7689@gmail.com>
2025-04-12 22:21:11 +0800

    fix: Add document to pagniation and using clippy::warn to main.rs

--- src/types/pagination.rs
+++ src/types/pagination.rs
@@ -1,13 +1,22 @@
+#![warn(clippy::all)]
+
 use std::collections::HashMap;
 
 use handle_errors::Error;
 
+/// Pagniation struct that is getting extracted
+/// from query params
 #[derive(Debug)]
 pub struct Pagniation {
     pub start: usize,
     pub end: usize,
 }
 
+/// Extract query paramters from the `/questions` route
+/// # Example Query
+/// Get requests to this route can have a pagniation attached so we just
+/// return the question we need
+/// `/questions?start=1&send=10`
 pub fn extract_pagniation(params: HashMap<String, String>) -> Result<Pagniation, Error> {
     if params.contains_key("start") && params.contains_key("end") {
         return Ok(Pagniation {
